6d7876e98691a2178bc602fc465fddb771b34d8e
Carlos Ramos <Carlos Ramos@DESKTOP-C6HRF5G>
2024-07-16 20:56:18 -0600

    Agregar archivos de proyecto.

---
+++ ejemplo_1_4-5/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Introduce tu nombre: ");
+
+string nombre = Console.ReadLine();
+
+Console.WriteLine("hola " + nombre);
+
+Console.ReadKey();
+
+
+Console.WriteLine("introduce tu ciudad");
+
+string ciudad = Console.ReadLine();
+
+Console.WriteLine("Hola " + nombre + " Bienvenido a " + ciudad);
+
+Console.ReadKey();
